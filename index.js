672daee2be12282781c9adb83a56d65b212264f3
Chubi <okafor.chinwuba@gmail.com>
2024-09-09 09:26:56 +0100

    create board function

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-
+document.addEventListener('DOMContentLoaded',function(){
+  createBoard(32)
+})
 
 function createBoard(size){
   let board = document.querySelector(".containers");
   board.style.gridTemplateColums=`repeat(${size}, 1fr)`;
-  board.style.gridTemplateRows="repeat(16, 1fr)";
+  board.style.gridTemplateRows=`repeat(${size}, 1fr)`;
+
+  let numDivs = size * size;
+
+  for (let i=0; i< numDivs; i++){
+    let div = document.createElement('div');
+    div.style.backgroundColor = "yellow"
+    board.insertAdjacentElement('beforeend', div)
+  }
 }
